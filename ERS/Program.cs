9c9c8d784fdfaa4e6c605a2440c270575e3cec5b
bgwcky <bryangewick@gmail.com>
2022-10-01 17:21:22 -0500

    created username specs

--- ERS/Program.cs
+++ ERS/Program.cs
@@ -1,16 +1,33 @@
-﻿
+﻿// Start screen
+    Console.WriteLine("_____________________________________________________");
+    Console.WriteLine("Welcome to the Revature Expense Reimbursement System!");
+    Console.WriteLine("_____________________________________________________");
+
+
  // Login/Register Branching
 
-    Console.WriteLine("Do you have an account already? Type Login or Register");
+
+
+
+    Console.WriteLine("Do you have an account already? Type \"login\" or \"register\" to continue. Type 0 to exit.");
     string? intro;
     intro = Console.ReadLine();
-    intro = intro.ToLower();
+    try {
+        intro = intro.ToLower();
+    } catch (System.NullReferenceException e) 
+    {
+        Console.WriteLine("Username must be at least 6 characters.");
+    }
 
     if (intro == "login")
     {
         Console.WriteLine("Welcome Back! Please enter your username and password");
-        
+
     
+    } else if (intro == "register")
+    {
+        Console.WriteLine("Let's create a new account! First, choose your username: (must contain 6 characters)");
+        string? username = Console.ReadLine();
     }
     
 
