6d4a9fd8b1ce078cf83a7c9aa1bb444193478142
bgwcky <bryangewick@gmail.com>
2022-10-06 11:56:57 -0500

    fixed assmembly errors temp by commenting out

--- ERS/UI/Program.cs
+++ ERS/UI/Program.cs
@@ -1,4 +1,5 @@
-﻿// Start screen
+﻿
+// Start screen
     
     Console.WriteLine("                                                     ");
     Console.WriteLine("/////////////////////////////////////////////////////");
@@ -7,11 +8,11 @@
     Console.WriteLine("                                                     ");
 
 
-
+    
 
     Console.WriteLine("Do you have an account already? Type \"login\" or \"register\" to continue. Type 0 to exit.");
-    var intro = "";
-    intro = Console.ReadLine();
+    string intro = "";
+    intro = Console.ReadLine()!.Trim().ToLower();
 
 
         try 
@@ -22,6 +23,7 @@
         } 
         catch (System.NullReferenceException e) 
         {
+            Console.WriteLine(e.Message);
 
         Console.WriteLine("Username must be at least 6 characters.");
         
@@ -30,11 +32,11 @@
         if (intro == "login")
         {
         Console.WriteLine("Welcome Back! Please enter your username.");
-        string? login = Console.ReadLine();
+        string login = Console.ReadLine()!.Trim().ToLower();
 
         
         Console.WriteLine("Great! Now enter your password");
-        string? pWord = Console.ReadLine();
+        string pWord = Console.ReadLine()!.Trim().ToLower();
         
         } 
         
@@ -42,10 +44,10 @@
         
         {
         Console.WriteLine("Let's create a new account! First, choose your username: (must be 1-6 characters)");
-        string? value = Console.ReadLine();
+        string value = Console.ReadLine()!.Trim().ToLower();
 
         Console.WriteLine("Great! Please enter a new password: (must be 8-12 characters)");
-        string? regPassword = Console.ReadLine();
+        string regPassword = Console.ReadLine()!.Trim().ToLower();
         } 
         else if (intro == "0")
         {
@@ -72,7 +74,7 @@
     System.Console.WriteLine("[account]: To view and update your account info");
     System.Console.WriteLine("[exit]: To exit the application");
 
-    string? input = Console.ReadLine();
+    string input = Console.ReadLine()!.Trim().ToLower();
 
     if (input == "process")
     {
@@ -85,18 +87,18 @@
 
 
 
-static string ApproveTicket(bool IsManager)
-    {
-        string result;
-        if (IsManager == true)
-        {
-            Console.WriteLine("Ticket approved!");
-            result = "Approved";
-        } else 
-        {
-            Console.WriteLine("Please enter Manager password if you are a manager.");
-            result = "Denied";
-        }
+// static string ApproveTicket(bool IsManager)
+//     {
+//         string result;
+//         if (IsManager == true)
+//         {
+//             Console.WriteLine("Ticket approved!");
+//             result = "Approved";
+//         } else 
+//         {
+//             Console.WriteLine("Please enter Manager password if you are a manager.");
+//             result = "Denied";
+//         }
         
-        return result;
-    }
+//         return result;
+//     }
